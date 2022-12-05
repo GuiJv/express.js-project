748b78e94da3bf51996c56e49973e53fe5dd2d83
guilherme <guilhermejosevinhas@gmail.com>
2022-12-05 15:09:42 -0300

    404

--- myapp/app.js
+++ myapp/app.js
@@ -3,9 +3,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/ping', (req, res) => {
+  res.status(200)
+  res.json('pong')
 })
+
+app.all('*', (req, res) => {
+  res.status(404)
+  res.json('Pagina não encontrada')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
